e1f66e4cdf9d68cf99b5f049c5c32cf9c883f0d3
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-08-17 07:01:04 +0200

    add color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        redFontColor: "hsl(14, 86%, 42%)",
+        darkRedColor: "hsl(14, 65%, 9%)",
+        secondTextColor: "hsl(12, 20%, 44%)",
+        boxColor: "hsl(20, 50%, 98%)",
+        backgroundPageColor: "hsl(14, 25%, 72%)",
+        penetratingWhite: "hsl(7, 20%, 60%)",
+      },
+    },
   },
   plugins: [],
 };
