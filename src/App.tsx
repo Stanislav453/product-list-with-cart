c48cf43213dcfc9d6cf7df31ce08098dc45d61f9
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-08-17 08:11:13 +0200

    remove log

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { useState } from "react";
-import  ProductsContainer  from "./componets/ProductsContainer";
+import ProductsContainer from "./componets/ProductsContainer";
 import CardContainer from "./componets/CardContainer";
 import { NewProductType } from "./type";
 
 export default function App() {
   const [productsInCard, setProductsInCard] = useState<NewProductType[]>([]);
-  console.log(productsInCard);
-  
-  
+  // console.log(productsInCard);
 
   return (
     <main className="flex gap-5 p-20">
