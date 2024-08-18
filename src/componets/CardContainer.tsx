8112bb7a12377243f09b03d09b45adfba33564a0
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-08-18 16:33:36 +0200

    add props type, create card switch if array fill, create removeCardItem func,

--- src/componets/CardContainer.tsx
+++ src/componets/CardContainer.tsx
@@ -1,38 +1,74 @@
-// import EmptyCard from "./EmptyCard";
-import removeIcon from "../../public/assets/images/icon-remove-item.svg"
-import { NewProductType } from "../type";
+import removeIcon from "../../public/assets/images/icon-remove-item.svg";
+import { type NewProductType } from "../type";
+import EmptyCard from "./EmptyCard";
 
 type CardContainerProps = {
   products: NewProductType[];
+  setProductsInCard: React.Dispatch<React.SetStateAction<NewProductType[]>>;
 };
 
-export default function CardContainer({ products }: CardContainerProps) {
+export default function CardContainer({
+  products,
+  setProductsInCard,
+}: CardContainerProps) {
+  let totalPrice = 0;
+
+  const removeCardListItem = (name: string) => {
+    setProductsInCard((prevState) => {
+      return prevState.filter((oneProduct) => oneProduct.name !== name);
+    });
+  };
+
   return (
-    <aside className="self-start w-full max-w-80 h-auto p-4 bg-boxColor rounded-2xl ">
+    <aside className="sticky top-[20px] self-start w-full max-w-80 h-auto p-4 bg-boxColor rounded-2xl ">
       <h2 className=" text-2xl font-bold text-redFontColor ">Your cart (0)</h2>
       <div className="p-2">
-        {/* <EmptyCard /> */}
-        <ul>
-          {products?.map((oneProduct: NewProductType, index: number) => {
-            const { name, price, count } = oneProduct;
-            const totalPrice = price * count;
-            
-            return (
-              <li key={index}>
-                <h2>{name}</h2>
-                <div>
-                  <p>
-                    <span>{count}</span>
-                    <span>${totalPrice.toFixed(2)}</span>
-                  </p>
-                  <button>
-                    <img src={removeIcon} alt="remove_item" />
-                  </button>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+        {products.length === 0 ? (
+          <EmptyCard />
+        ) : (
+          <>
+            <ul className="flex flex-col gap-4">
+              {products?.map((oneProduct: NewProductType, index: number) => {
+                const { name, price, count } = oneProduct;
+                const productTotalPrice = price * count;
+                totalPrice += productTotalPrice;
+
+                return (
+                  <li className="border-b-[1px] pb-4" key={index}>
+                    <h2 className="text-black font-medium">{name}</h2>
+                    <div className="flex justify-between">
+                      <p>
+                        <span className="text-sm text-redFontColor">
+                          {count}x
+                        </span>
+                        <span className="text-sm pl-3 text-backgroundPageColor">
+                          <span className="text-[10px]">@</span>$
+                          {price.toFixed(2)}
+                        </span>
+                        <span className="text-sm pl-2 text-penetratingWhite">
+                          ${productTotalPrice.toFixed(2)}
+                        </span>
+                      </p>
+                      <button onClick={() => removeCardListItem(name)}>
+                        <img
+                          className="border-2 p-1 rounded-full"
+                          src={removeIcon}
+                          alt="remove_item"
+                        />
+                      </button>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+            <p className="flex justify-between py-4 ">
+              <span>Order total</span>
+              <span className="font-bold text-xl">
+                ${totalPrice.toFixed(2)}
+              </span>
+            </p>
+          </>
+        )}
       </div>
     </aside>
   );
