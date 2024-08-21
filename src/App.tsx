0f68cee6e9037d0d4a251e961c407eb93561b79a
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-08-21 19:30:17 +0200

    create order state

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import ProductsContainer from "./componets/ProductsContainer";
 import CardContainer from "./componets/CardContainer";
 import { NewProductType } from "./type";
+import { Order } from "./componets/Order";
 
 export default function App() {
   const [productsInCard, setProductsInCard] = useState<NewProductType[]>([]);
-
+  const [isOrderActive, setIsOrderActive] = useState(false);
+  console.log(isOrderActive);
 
   return (
-    <main className="flex gap-5 p-20">
+    <section className="flex flex-wrap justify-center gap-4 p-20">
       <ProductsContainer
         productsInCard={productsInCard}
         setProductsInCard={setProductsInCard}
       />
+
       <CardContainer
+        setIsOrderActive={setIsOrderActive}
         products={productsInCard}
         setProductsInCard={setProductsInCard}
       />
-    </main>
+      <Order
+        setIsOrderActive={setIsOrderActive}
+        isOrderActive={isOrderActive}
+        orderProducts={productsInCard}
+        setProductsInCard={setProductsInCard}
+      />
+    </section>
   );
 }
