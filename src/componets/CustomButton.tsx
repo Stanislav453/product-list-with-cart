94b670ee080ba5bf40cbe101eac92da5c944530e
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-08-21 19:31:21 +0200

    add onClick action

--- src/componets/CustomButton.tsx
+++ src/componets/CustomButton.tsx
@@ -3,11 +3,17 @@ import { ReactNode } from "react";
 type CustomButtonProps = {
   children: ReactNode;
   className: string;
+  onClick?: () => void;
 };
 
-export const CustomButton = ({ className, children }: CustomButtonProps) => {
+export const CustomButton = ({
+  className,
+  children,
+  onClick,
+}: CustomButtonProps) => {
   return (
     <button
+      onClick={onClick}
       className={`
         w-full p-3 text-white bg-redFontColor rounded-full hover:bg-red-800
         ${className}
